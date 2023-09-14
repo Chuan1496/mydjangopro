a02d62f518030be6900a79cccd1fb6cf81308072
jxw1496 <jxw1496@student.bham.ac.uk>
2023-09-14 08:54:02 +0800

    InventorySystem

--- product/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class ProductForm(forms.Form):
-    id = forms.IntegerField(required=False)
-    selectedCate = forms.CharField(required=False)
-    selectedBrand = forms.CharField(required=False)
-    selectedSup = forms.CharField(required=False)
-    prodName = forms.CharField(required=False)
